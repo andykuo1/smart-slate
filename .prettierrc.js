1adb18880825a79f9066512222b564f0db9c2b94
andykuo1 <andykuo1supergreen@gmail.com>
2023-12-01 06:43:52 -0800

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSameLine: true,
+  importOrder: ['react', '<THIRD_PARTY_MODULES>', '^@/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
