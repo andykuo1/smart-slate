85c11a609646eb4b7526c0febd40904b378ca09a
andykuo1 <andykuo1supergreen@gmail.com>
2023-12-13 19:13:54 -0800

    Add pwa support

---
+++ pwa-assets.config.js
@@ -0,0 +1,16 @@
+import {
+  createAppleSplashScreens,
+  defineConfig,
+  minimal2023Preset,
+} from '@vite-pwa/assets-generator/config';
+
+export default defineConfig({
+  headLinkOptions: {
+    preset: '2023',
+  },
+  preset: {
+    ...minimal2023Preset,
+    appleSplashScreens: createAppleSplashScreens(),
+  },
+  images: ['public/logo.svg'],
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react-swc';
 
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -15,5 +16,57 @@ export default defineConfig({
       include: '**/*.svg',
     }),
     tsconfigPaths(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      injectRegister: 'inline',
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'EagleStudio SmartSlate',
+        short_name: 'SmartSlate',
+        description: 'A shot list made with ❤️',
+        theme_color: '#ffffff',
+        screenshots: [
+          {
+            src: '/smart-slate/demo1.png',
+            sizes: '1098x942',
+            type: 'image/png',
+            form_factor: 'wide',
+            label: 'My Movie shot list',
+          },
+          {
+            src: '/smart-slate/demo2.png',
+            sizes: '518x636',
+            type: 'image/png',
+            label: 'Recording a new take',
+          },
+        ],
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
   ],
 });
